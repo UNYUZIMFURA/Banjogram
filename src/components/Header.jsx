7c750f62e9c2e8f63ec667cb6a1cbef96b712fd2
UNYUZIMFURA <banjoflylive@gmail.com>
2023-04-25 07:58:05 +0200

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="flex items-center gap-4 h-[7.5vh] bg-violet-200 px-4">
       <form className="bg-[red]">
-        <input className="h-[2rem]"/>
+        <input className="h-[2rem] bg-[red]"/>
       </form>
     </div>
   );
