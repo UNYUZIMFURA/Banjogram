c30085623052ce6a5173bb440b355f97bf35816a
UNYUZIMFURA <banjoflylive@gmail.com>
2023-04-25 08:20:00 +0200

    [Style] ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { FiHeart } from "react-icons/fi";
+import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
     <div className="flex items-center gap-4 h-[7.5vh] border-b px-4">
       <form className="flex items-center w-full justify-around">
-        <input className="h-[2rem] outline-none indent-8" />
-        <FiHeart color="white" size={25}/>
+        <div className="h-[2rem] outline-none indent-8 bg-white w-[80%] flex items-center justify-around">
+          <CiSearch />
+          <input className="bg-[red] w-[70%]" />
+        </div>
+        <FiHeart color="white" size={25} />
       </form>
     </div>
   );
