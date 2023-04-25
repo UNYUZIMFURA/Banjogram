b35466c2be65af7ee823ccd5aca82c67efdad862
UNYUZIMFURA <banjoflylive@gmail.com>
2023-04-25 10:25:31 +0200

    [Feat]: Trying to add fetched images

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,10 +1,27 @@
 import Story from "./Story";
+const { useEffect, useState } = require("react");
 
 const Stories = () => {
-    return(
-        <div className="h-[7rem] flex items-center border border-[#3E4042]">
-            <Story />
-        </div>
-    )
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    if (images.length != 0) {
+      console.log(images[0].urls.small);
+    }
+  }, [images]);
+  useEffect(() => {
+    const fetchPics = async () => {
+      const res = await fetch(
+        "https://api.unsplash.com/photos/?client_id=1LL76bQsvQAoX-PbSVsga7ELNj8V6crx_DC2yFFljwU"
+      );
+      const data = await res.json();
+      setImages(data);
+    };
+    fetchPics();
+  }, []);
+  return (
+    <div className="flex h-[7rem] items-center border border-[#3E4042]">
+      {images.length != 0 ? images.map((img) => <Story />) : null}
+    </div>
+  );
 };
-export default Stories
+export default Stories;
