a00beaba92e9bb20678b0c900d817a5665691167
UNYUZIMFURA <banjoflylive@gmail.com>
2023-04-26 05:40:25 +0200

    [Style]: Stories customization

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,12 +1,23 @@
-import Story from "./Story"
+import Story from "./Story";
 
 const Stories = () => {
-  const storyNum = [1,2,3,4,5,6,7,8,9,0];
+  const stories = [
+    { id: 1, userName: "element_eleeeh" },
+    { id: 2, userName: "k.mbappe" },
+    { id: 3, userName: "paulkagame" },
+    { id: 4, userName: "kallehallden" },
+    { id: 5, userName: "psg" },
+    { id: 6, userName: "kevin_banjo" },
+    { id: 7, userName: "neymarjr" },
+    { id: 8, userName: "_rl9" },
+    { id: 9, userName: "therock" },
+    { id: 0, userName: "rocky_kimomo" },
+  ];
   return (
-    <div className="flex h-[7rem] items-center overflow-scroll scrollbar-hide px-2">
-      {
-        storyNum.map(item => <Story id={item} key={item}/>)
-      }
+    <div className="flex h-[7rem] items-center overflow-scroll border px-2 scrollbar-hide">
+      {stories.map((item) => (
+        <Story id={item} key={item.id} storyUser={item.userName} />
+      ))}
     </div>
   );
 };
--- src/components/Stories/Story.jsx
+++ src/components/Stories/Story.jsx
@@ -3,16 +3,19 @@ import "./styles/story.css";
 const Story = (props) => {
   const randomString = Math.random();
   return (
-    <div
-      className={`story ${
-        props.id == 1 ? "ml-0" : "ml-4"
-      } flex h-[4rem] min-w-[4rem]  cursor-pointer`}
-    >
-      <img
-        src={`https://picsum.photos/400/400?random=${randomString}`}
-        alt="..."
-        className="mx-auto my-auto h-[92%] rounded-full"
-      />
+    <div className={`h-[10rem] w-[10rem] ${
+      props.id == 1 ? "ml-0" : "ml-4"
+    } flex flex-col items-center justify-center bg-red-100`}>
+      <div
+        className={`story  flex h-[4rem] min-w-[4rem] cursor-pointer bg-red-100`}
+      >
+        <img
+          src={`https://picsum.photos/400/400?random=${randomString}`}
+          alt="..."
+          className="mx-auto my-auto h-[92%] rounded-full"
+        />
+      </div>
+      <span></span>
     </div>
   );
 };
