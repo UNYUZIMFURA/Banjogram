85045104cc6bed25dc3bfa99b2c36a7263d19ec6
UNYUZIMFURA <banjoflylive@gmail.com>
2023-04-26 06:10:04 +0200

    [Style]: Restructuring Stories

--- src/components/Stories/Story.jsx
+++ src/components/Stories/Story.jsx
@@ -3,19 +3,21 @@ import "./styles/story.css";
 const Story = (props) => {
   const randomString = Math.random();
   return (
-    <div className={`h-[10rem] w-[4rem] ${
-      props.id == 1 ? "ml-0" : "ml-4"
-    } flex flex-col items-center justify-center bg-red-100 gap-2`}>
-      <div
-        className={`story  flex h-[4rem] min-w-[4rem] cursor-pointer`}
-      >
+    <div
+      className={`h-[10rem] w-[4rem] ${
+        props.id === 1 ? "ml-0" : "ml-4"
+      } overflow-hidde flex flex-col items-center justify-center gap-2`}
+    >
+      <div className={`story  flex h-[4rem] min-w-[4rem] cursor-pointer`}>
         <img
           src={`https://picsum.photos/400/400?random=${randomString}`}
           alt="..."
           className="mx-auto my-auto h-[92%] rounded-full"
         />
-      </div>
-      <span className="text-[0.7rem]">{props.userName}</span>
+      </div>  
+      <span className="text-white max-w-[4rem] text-[0.7rem] overflow-hidden">
+        {props.userName}
+      </span>
     </div>
   );
 };
