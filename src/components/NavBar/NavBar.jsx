ce8c2f606d64edf366331ba91c290ab7d76e36e9
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-11 09:32:17 +0200

    [...]: Perfect Fit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,9 @@
 const NavBar = () => {
-    
-}
+  return (
+    <div className="w-full fixed h-[3rem] flex items-center gap-3 bg-[red] bottom-0">
+     
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
-import Stories from "../components/Stories/Stories"
+import Stories from "../components/Stories/Stories";
 import Posts from "../components/Posts/Posts";
+import NavBar from "../components/NavBar/NavBar";
 
 const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ const Homepage = () => {
       <Header />
       <Stories />
       <Posts />
+      <NavBar />
     </div>
   );
 };
