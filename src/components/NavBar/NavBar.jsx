e29e6ac74b38520c45d5c901d0a8cdd2bd103051
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-12 09:57:23 +0200

    ...

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,17 +7,17 @@ import { TbSend } from "react-icons/tb";
 const NavBar = () => {
   const randomString = Math.random();
   return (
-    <div className="fixed bottom-0 flex h-[3rem] w-full items-center justify-around bg-black">
-      <AiFillHome size={27} fill="white" cursor="pointer"/>
-      <FaRegCompass size={26} fill="white" cursor="pointer"/>
-      <BiMoviePlay size={28} fill="white" cursor="pointer"/>
-      <CgAddR size={27} color="white" cursor="pointer"/>
-      <TbSend size={27} color="white" cursor="pointer"/>
-      <div className="h-7 w-7 rounded-full bg-slate-500">
+    <div className="fixed bottom-0 flex h-[3rem] w-full items-center justify-around border-t border-[#303030] bg-black">
+      <AiFillHome size={25} fill="white" cursor="pointer" />
+      <FaRegCompass size={24} fill="white" cursor="pointer" />
+      <BiMoviePlay size={26} fill="white" cursor="pointer" />
+      <CgAddR size={25} color="white" cursor="pointer" />
+      <TbSend size={25} color="white" cursor="pointer" />
+      <div className="h-6 w-6 rounded-full bg-slate-500">
         <img
           src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
           alt=""
-          className="rounded-full cursor-pointer"
+          className="cursor-pointer rounded-full"
         />
       </div>
     </div>
