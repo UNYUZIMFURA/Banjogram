caee3ce51052c49a358bb7f2fb842a74e98142ce
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-12 09:30:00 +0200

    [Style]: WWork on NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,18 @@
 import { AiFillHome } from "react-icons/ai";
 import { FaRegCompass } from "react-icons/fa";
-import {BiMoviePlay} from "react-icons/bi"
-import {CgAddR} from "react-icons/cg"
+import { BiMoviePlay } from "react-icons/bi";
+import { CgAddR } from "react-icons/cg";
 import { TbSend } from "react-icons/tb";
 
 const NavBar = () => {
   return (
     <div className="fixed bottom-0 flex h-[3rem] w-full items-center gap-3 bg-blue-200">
-      <AiFillHome />
-      <FaRegCompass />
-      <BiMoviePlay />
-      <CgAddR />
-      <TbSend />
+      <AiFillHome size={30} />
+      <FaRegCompass size={30} />
+      <BiMoviePlay size={30} />
+      <CgAddR size={30} />
+      <TbSend size={30} />
+      <div className="h-8 w-8 rounded-full bg-slate-500"></div>
     </div>
   );
 };
