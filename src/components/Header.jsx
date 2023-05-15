122dbba47b1ee335847a6c0c5c564cfbf7c8b463
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-15 11:28:58 +0200

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <div className="flex h-[8vh] items-center justify-between px-8 border-b border-[#3E4042]">
+    <div className="flex h-[8vh] items-center justify-center min-[470px]:justify-between px-8 border-b border-[#3E4042]">
       <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[470px]:flex">
         Banjogram
       </span>
-      <div className="flex items-center h-full gap-6 px-2 ">
+      <div className="flex items-center h-full gap-6 px-2">
         <form className="w-68 flex h-[2.3rem] items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
           <CiSearch size={20} color="#a1a1a1" cursor="pointer" />
           <input
