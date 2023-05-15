8d3f7a7a07d11df6226c9a573e5bfad5b75840f6
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-15 06:30:28 +0200

    [Style]: Customizing my Logo to Banjogram ^_^

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042] min-[410px]:justify-between min-[410px]:px-4">
       <span className="font-satisfy hidden cursor-pointer text-white min-[470px]:flex">
-        Instagram
+        Banjogram
       </span>
       <form className="w-68 flex h-[2.3rem] items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
         <CiSearch size={20} color="#a1a1a1" />
