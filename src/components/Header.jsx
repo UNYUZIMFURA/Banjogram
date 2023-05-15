959e2fca0b8d2be3be2cdd315c9ca3181008aa32
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-15 08:18:28 +0200

    [...]: Restructuring Every Layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042] min-[410px]:justify-between min-[410px]:px-4">
+    <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042]">
       <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[470px]:flex">
         Banjogram
       </span>
-      <div>
+      <div className="flex items-center h-full gap-6 px-2">
         <form className="w-68 flex h-[2.3rem] items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
           <CiSearch size={20} color="#a1a1a1" cursor="pointer" />
           <input
