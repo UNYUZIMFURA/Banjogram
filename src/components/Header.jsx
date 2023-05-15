d02932bcd80eb3e823d6caaef02a976e20cb10a5
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-15 08:04:46 +0200

    [Style]: Nice Looking view at breakpoint of w-[500px]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,19 @@ import { CiSearch } from "react-icons/ci";
 const Header = () => {
   return (
     <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042] min-[410px]:justify-between min-[410px]:px-4">
-      <span className="font-satisfy hidden cursor-pointer text-white min-[470px]:flex text-2xl">
+      <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[470px]:flex">
         Banjogram
       </span>
-      <form className="w-68 flex h-[2.3rem] items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
-        <CiSearch size={20} color="#a1a1a1" cursor="pointer" />
-        <input
-          className="h-full w-[80%] bg-[#272727] text-white outline-none placeholder:font-light placeholder:text-[#a1a1a1]"
-          placeholder="Search"
-        />
-      </form>
-      <FiHeart color="white" size={25} cursor="pointer" />
+      <div>
+        <form className="w-68 flex h-[2.3rem] items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
+          <CiSearch size={20} color="#a1a1a1" cursor="pointer" />
+          <input
+            className="h-full w-[80%] bg-[#272727] text-white outline-none placeholder:font-light placeholder:text-[#a1a1a1]"
+            placeholder="Search"
+          />
+        </form>
+        <FiHeart color="white" size={25} cursor="pointer" />
+      </div>
     </div>
   );
 };
