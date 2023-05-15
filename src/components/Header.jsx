dcc6d11c565b1ebc7a2396f82b2e1d9f557f08a7
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-15 05:47:32 +0200

    [Style]: Show Instagram Logo on breakpoint of [470px]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042]">
+    <div className="flex h-[8vh] items-center justify-around border-b border-[#3E4042] min-[410px]:justify-between min-[410px]:px-4">
+      <span className="text-white hidden min-[470px]:flex">Instagram</span>
       <form className="flex h-[2.3rem] w-68 items-center justify-evenly rounded-md bg-[#272727] indent-8 outline-none">
         <CiSearch size={20} color="#a1a1a1" />
         <input
