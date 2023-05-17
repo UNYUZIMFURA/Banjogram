b6fcc180540188e59bae8d08736b3ffcd9e069fd
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-17 09:18:31 +0200

    [Structure]: Final Base Structure

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,14 +6,14 @@ import Suggested from "../components/Suggested/Suggested";
 
 const Homepage = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center bg-black md:items-center md:justify-center">
-      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex">
-        <div>
+    <div className="flex h-screen w-full flex-col items-center bg-black md:items-center md:justify-center min-[1160px]:items-end">
+      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row bg-green-200 min-[1160px]:justify-around min-[1160px]:w-[90%]">
+        <div className="bg-red-200">
           <Header />
           <Stories />
           <Posts />
         </div>
-        {/* <Suggested /> */}
+        <Suggested />
       </div>
       <NavBar />
     </div>
