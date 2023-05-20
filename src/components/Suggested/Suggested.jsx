0c46f56cbbb3ee03742c8806f13eeaffa584441a
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-20 23:53:49 +0200

    ...

--- src/components/Suggested/Suggested.jsx
+++ src/components/Suggested/Suggested.jsx
@@ -1,6 +1,8 @@
 const Suggested = () => {
 return (
-    <div className="h-[30rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex"></div>
+    <div className="h-[30rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex">
+        Suggested
+    </div>
 )
 }
 
