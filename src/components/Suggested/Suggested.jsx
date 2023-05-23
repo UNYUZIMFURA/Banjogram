a2085182b447eae771ad388b528c118d15f75822
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-23 11:26:51 +0200

    [...]: Back in action!

--- src/components/Suggested/Suggested.jsx
+++ src/components/Suggested/Suggested.jsx
@@ -1,7 +1,7 @@
 const Suggested = () => {
 return (
     <div className="h-[30rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex">
-        <div className="flex"></div>
+        <div className="flex h-[4rem] w-full bg-red-200"></div>
     </div>
 )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ import Suggested from "../components/Suggested/Suggested";
 const Homepage = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-black md:items-center md:justify-center min-[1160px]:items-end">
-      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:justify-around min-[1160px]:w-[90%]">
+      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row bg-green-400 min-[1160px]:items-start min-[1160px]:justify-around min-[1160px]:w-[90%]">
         <div className="">
           <Header />
           <Stories />
