0ea98aae050b999352e442ac7237d98def1f7d5e
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-23 11:39:51 +0200

    [Fix]: Fix Design Issues regarding storyDiv overflowing, [Set Width of Parent Div]

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -14,7 +14,7 @@ const Stories = () => {
     { id: 0, userName: "rocky_kimomo" },
   ];
   return (
-    <div className="flex h-32 bg-red-200 items-center overflow-scroll px-2 scrollbar-hide min-[630px]:w-[40rem]">
+    <div className="flex h-32 items-center overflow-scroll px-2 scrollbar-hide min-[630px]:w-[40rem]">
       {stories.map((item) => (
         <Story id={item.id} key={item.id} userName={item.userName} />
       ))}
