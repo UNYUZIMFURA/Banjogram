1f372aa419aa784b62f27c7ccfed05b8528ac941
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-23 11:39:17 +0200

    [Fix]: Fix Design Issues regarding storyDiv overflowing

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -8,7 +8,7 @@ const Posts = () => {
     { id: 4, userName: "kallehallden" },
   ];
   return (
-    <div className="bg-blue-200 flex h-[70vh] w-full flex-col items-center gap-[3rem] overflow-scroll scrollbar-hide">
+    <div className="flex h-[70vh] w-full flex-col items-center gap-[3rem] overflow-scroll scrollbar-hide">
       {posts.map((post) => (
         <Post id={post.id} key={post.id} />
       ))}
