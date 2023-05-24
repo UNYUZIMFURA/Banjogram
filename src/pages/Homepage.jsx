d5b65b9db464b1b1473781039f3cf275fdfe4408
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-24 15:56:46 +0200

    [Style]: Find space to fit Recommended div

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,8 +7,8 @@ import Suggested from "../components/Suggested/Suggested";
 const Homepage = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end">
-      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row zmin-[1160px]:justify-around min-[1160px]:w-[90%] bg-gray-40">
-        <div className="w-full flex flex-col items-center">
+      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%]">
+        <div className="w-full flex flex-col items-center min-[1160px]:w-fit">
           <Header />
           <Stories />
           <Posts />
