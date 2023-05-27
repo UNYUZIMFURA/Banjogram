396de44717cdb6b9555ea576d024575cfecb7ad7
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-27 23:12:58 +0200

    [Style]: Code Refactoring of the Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,23 +3,25 @@ import { FaRegCompass } from "react-icons/fa";
 import { BiMoviePlay } from "react-icons/bi";
 import { CgAddR } from "react-icons/cg";
 import { TbSend } from "react-icons/tb";
-import {BsInstagram} from "react-icons/bs"
+import { BsInstagram } from "react-icons/bs";
 
 const NavBar = () => {
   const randomString = Math.random();
   return (
-    <div className="fixed bottom-0 flex h-[3rem] w-full items-center justify-around border-t border-[#303030] md:top-0 md:flex-col md:h-screen md:w-[4.5rem] md:left-0 md:border-r">
-      <AiFillHome size={25} fill="white" cursor="pointer" />
-      <FaRegCompass size={24} fill="white" cursor="pointer" />
-      <BiMoviePlay size={26} fill="white" cursor="pointer" />
-      <CgAddR size={25} color="white" cursor="pointer" />
-      <TbSend size={25} color="white" cursor="pointer" />
-      <div className="h-6 w-6 rounded-full ">
-        <img
-          src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
-          alt=""
-          className="cursor-pointer rounded-full"
-        />
+    <div className="fixed bottom-0 flex h-[3rem] w-full items-center justify-around border-t border-[#303030] md:left-0 md:top-0 md:h-screen md:w-[4.5rem] md:flex-col md:border-r">
+      <div className="bg-red-100 w-full h-full flex items-center justify-around">
+        <AiFillHome size={25} fill="white" cursor="pointer" />
+        <FaRegCompass size={24} fill="white" cursor="pointer" />
+        <BiMoviePlay size={26} fill="white" cursor="pointer" />
+        <CgAddR size={25} color="white" cursor="pointer" />
+        <TbSend size={25} color="white" cursor="pointer" />
+        <div className="h-6 w-6 rounded-full ">
+          <img
+            src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
+            alt=""
+            className="cursor-pointer rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
