6720bd1c32dc87bc0d4efc4d9a6983cf349c9783
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-28 19:17:29 +0200

    [Add]: Continue working on Recommended Section

--- src/components/Suggested/ProfileDetails.jsx
+++ src/components/Suggested/ProfileDetails.jsx
@@ -1,14 +1,16 @@
 const ProfileDetails = () => {
-return(
-    <div className="flex h-16 w-full bg-red-200 items-center">
+  return (
+    <div className="flex h-16 w-full items-center bg-red-200">
+      <div className="flex">
         <div className="h-12 w-12 rounded-full bg-green-200"></div>
         <div className="flex flex-col">
-            <span>joykevinrobin</span>
-            <span>JOY KEVIN ROBIN</span>
+          <span>joykevinrobin</span>
+          <span>JOY KEVIN ROBIN</span>
         </div>
-        <span className="text-sm">Switch</span>
+      </div>
+      <span className="text-sm">Switch</span>
     </div>
-)
-}
+  );
+};
 
-export default ProfileDetails
+export default ProfileDetails;
