14754f7684916768e7a3eb43ab8f729d37e58bc6
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-28 17:39:39 +0200

    [Style]: Create base structure of Recommended Section

--- src/components/Suggested/Suggested.jsx
+++ src/components/Suggested/Suggested.jsx
@@ -1,7 +1,9 @@
+import ProfileDetails from "./ProfileDetails"
+
 const Suggested = () => {
 return (
     <div className="h-[25rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex mt-6">
-        <div className="flex h-[4rem] w-full bg-red-200"></div>
+        <ProfileDetails />
     </div>
 )
 }
