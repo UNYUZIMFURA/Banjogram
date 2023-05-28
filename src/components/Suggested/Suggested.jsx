eb3aac80d3080104d9f8abf9cc0629df1f9db669
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-28 10:43:35 +0200

    [Style]: Responsive Design of Navbar ~ 60% complete!

--- src/components/Suggested/Suggested.jsx
+++ src/components/Suggested/Suggested.jsx
@@ -1,6 +1,6 @@
 const Suggested = () => {
 return (
-    <div className="h-[30rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex mt-6">
+    <div className="h-[25rem] w-[20rem] bg-slate-500 hidden min-[1160px]:flex mt-6">
         <div className="flex h-[4rem] w-full bg-red-200"></div>
     </div>
 )
