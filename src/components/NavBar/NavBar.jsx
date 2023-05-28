451ebd6b2775c85d4001e83940d926d49b98ebba
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-28 10:13:05 +0200

    [Style]: Responsive Design the Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,10 +9,10 @@ import {RiMenuFill} from "react-icons/ri"
 const NavBar = () => {
   const randomString = Math.random();
   return (
-    <div className="fixed bottom-0 h-[3rem] border-t border-[#303030] md:left-0 md:top-0 md:h-screen md:w-[4.5rem] md:border-r md:flex md:flex-col md:items-center md:gap-8">
-      <div className="flex items-center h-full w-full md:flex-col bg-green-300 md:justify-around md:h-[60%]">
-        <BsInstagram size={25} fill="white" cursor="pointer" className="hidden md:flex"/>
-        <div className="flex h-full w-full items-center justify-around bg-red-200 md:flex-col md:justify-center md:gap-8 md:h-fit">
+    <div className="fixed w-full bottom-0 h-[3rem] border-t border-[#303030] md:left-0 md:top-0 md:h-screen md:w-[4.5rem] md:border-r md:flex md:flex-col md:items-center">
+      <div className="flex items-center h-full w-full md:flex-col bg-green-300 md:py-8 md:h-fit md:gap-12">
+        <BsInstagram size={23} fill="white" cursor="pointer" className="hidden md:flex"/>
+        <div className="flex h-full w-full items-center justify-around bg-red-200 md:flex-col md:justify-center md:gap-10 md:h-fit">
           <AiFillHome size={25} fill="white" cursor="pointer" />
           <FaRegCompass size={24} fill="white" cursor="pointer" />
           <BiMoviePlay size={26} fill="white" cursor="pointer" />
@@ -27,7 +27,7 @@ const NavBar = () => {
           </div>
         </div>
       </div>
-      <RiMenuFill fill="white" size={25} cursor="pointer hidden md:flex"/>
+      <RiMenuFill fill="white" size={25} className="cursor-pointer hidden md:flex"/>
     </div>
   );
 };
