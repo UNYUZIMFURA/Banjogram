906aea08d811b9ef52226b60f424316696aa1b15
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-30 16:46:12 +0200

    [Style]: Responsive Design Navbar for Tablets

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,17 +10,20 @@ const NavBar = () => {
   const randomString = Math.random();
   return (
     <div className="fixed bottom-0 h-12 w-full border-t border-[#303030] md:left-0 md:top-0 md:flex md:h-screen md:w-[4.5rem] md:flex-col md:items-center md:gap-64 md:border-r min-[1264px]:static min-[1264px]:w-[17rem]">
-      <div className="flex h-full w-full items-center md:h-fit md:flex-col md:gap-12 md:py-8">
+      <div className="flex h-full w-full items-center md:h-fit md:flex-col md:gap-12 md:py-8 bg-green-200 min-[1264px]:items-start min-[1264px]:px-6">
         <BsInstagram
           size={23}
           fill="white"
           cursor="pointer"
-          className="hidden md:flex"
+          className="hidden md:flex min-[1264px]:hidden"
         />
-        <div className="bg-red-200 flex h-full w-full items-center justify-around md:h-fit md:flex-col md:justify-center md:gap-10 min-[1264px]:items-start min-[1264px]:px-6">
-          <div className="flex gap-4 bg-red-500 i">
+        <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[1264px]:flex">
+          Banjogram
+        </span>
+        <div className="flex h-full w-full items-center justify-around bg-red-200 md:h-fit md:flex-col md:justify-center md:gap-10 min-[1264px]:items-start">
+          <div className="flex gap-4 bg-red-500">
             <AiFillHome size={25} fill="white" cursor="pointer" />
-            <span className="text-white hidden min-[1264px]:flex">Home</span>
+            <span className="hidden text-white min-[1264px]:flex">Home</span>
           </div>
           <div>
             <FaRegCompass size={24} fill="white" cursor="pointer" />
