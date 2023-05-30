81140d191eb3492b0d2b8dd57c87f33b583969a5
UNYUZIMFURA <banjoflylive@gmail.com>
2023-05-30 16:37:03 +0200

    [Style]: Responsive Design Navbar for Tablets

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,20 +4,36 @@ import { BiMoviePlay } from "react-icons/bi";
 import { CgAddR } from "react-icons/cg";
 import { TbSend } from "react-icons/tb";
 import { BsInstagram } from "react-icons/bs";
-import {RiMenuFill} from "react-icons/ri"
+import { RiMenuFill } from "react-icons/ri";
 
 const NavBar = () => {
   const randomString = Math.random();
   return (
-    <div className="fixed w-full bottom-0 h-12 border-t border-[#303030] md:left-0 md:top-0 md:h-screen md:w-[4.5rem] md:border-r md:flex md:flex-col md:items-center md:gap-64 min-[1264px]:static min-[1264px]:w-[17rem]">
-      <div className="flex items-center h-full w-full md:flex-col md:py-8 md:h-fit md:gap-12">
-        <BsInstagram size={23} fill="white" cursor="pointer" className="hidden md:flex"/>
-        <div className="flex h-full w-full items-center justify-around md:flex-col md:justify-center md:gap-10 md:h-fit">
-          <AiFillHome size={25} fill="white" cursor="pointer" />
-          <FaRegCompass size={24} fill="white" cursor="pointer" />
-          <BiMoviePlay size={26} fill="white" cursor="pointer" />
-          <CgAddR size={25} color="white" cursor="pointer" />
-          <TbSend size={25} color="white" cursor="pointer" />
+    <div className="fixed bottom-0 h-12 w-full border-t border-[#303030] md:left-0 md:top-0 md:flex md:h-screen md:w-[4.5rem] md:flex-col md:items-center md:gap-64 md:border-r min-[1264px]:static min-[1264px]:w-[17rem]">
+      <div className="flex h-full w-full items-center md:h-fit md:flex-col md:gap-12 md:py-8">
+        <BsInstagram
+          size={23}
+          fill="white"
+          cursor="pointer"
+          className="hidden md:flex"
+        />
+        <div className="bg-red-200 flex h-full w-full items-center justify-around md:h-fit md:flex-col md:justify-center md:gap-10 min-[1264px]:items-start min-[1264px]:px-6">
+          <div className="flex gap-4 bg-red-500 i">
+            <AiFillHome size={25} fill="white" cursor="pointer" />
+            <span className="text-white hidden min-[1264px]:flex">Home</span>
+          </div>
+          <div>
+            <FaRegCompass size={24} fill="white" cursor="pointer" />
+          </div>
+          <div>
+            <BiMoviePlay size={26} fill="white" cursor="pointer" />
+          </div>
+          <div>
+            <CgAddR size={25} color="white" cursor="pointer" />
+          </div>
+          <div>
+            <TbSend size={25} color="white" cursor="pointer" />
+          </div>
           <div className="h-6 w-6 rounded-full ">
             <img
               src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
@@ -27,7 +43,11 @@ const NavBar = () => {
           </div>
         </div>
       </div>
-      <RiMenuFill fill="white" size={25} className="cursor-pointer hidden md:flex"/>
+      <RiMenuFill
+        fill="white"
+        size={25}
+        className="hidden cursor-pointer md:flex"
+      />
     </div>
   );
 };
