64d7d944a6eaa1095a3dec8409ad22ca3c4d3225
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-01 21:07:59 +0200

    [Style]: Responsive Design of Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,18 +11,18 @@ const NavBar = () => {
   const randomString = Math.random();
   return (
     <div className="fixed bottom-0 h-12 w-full border-t border-[#303030] md:left-0 md:top-0 md:flex md:h-screen md:w-[4.5rem] md:flex-col md:items-center md:gap-64 md:border-r min-[1264px]:static min-[1264px]:w-[17rem]">
-      <div className="flex h-full w-full items-center md:h-fit md:flex-col md:gap-12 md:py-8 min-[1264px]:items-start bg-slate-20">
+      <div className="bg-slate-20 flex h-full w-full items-center md:h-fit md:flex-col md:gap-12 md:py-8 min-[1264px]:items-start">
         <BsInstagram
           size={23}
           fill="white"
           cursor="pointer"
           className="hidden md:flex min-[1264px]:hidden"
         />
-        <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[1264px]:flex min-[1264px]:ml-8">
+        <span className="hidden cursor-pointer font-satisfy text-2xl text-white min-[1264px]:ml-8 min-[1264px]:flex">
           Banjogram
         </span>
         <div className="flex h-full w-full items-center justify-around md:h-fit md:flex-col md:justify-center md:gap-10 min-[1264px]:items-start">
-          <div className="flex gap-4 min-[1264px] min-[1264px]:p-2 min-[1264px]:pl-8 bg-red-100 min-[1264px]:w-full">
+          <div className="min-[1264px] flex gap-4 bg-red-100 min-[1264px]:w-full min-[1264px]:p-2 min-[1264px]:pl-8">
             <AiFillHome size={25} fill="white" cursor="pointer" />
             <span className="hidden text-white min-[1264px]:flex">Home</span>
           </div>
@@ -48,12 +48,14 @@ const NavBar = () => {
               Messages
             </span>
           </div>
-          <div className="pl-8 h-6 w-6 gap-4  rounded-full min-[1264px]:flex">
-            <img
-              src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
-              alt=""
-              className="cursor-pointer rounded-full"
-            />
+          <div className="flex gap-4 min-[1264px]:pl-8">
+            <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-slate-200">
+              <img
+                src={`${process.env.REACT_APP_IMAGES_ENDPOINT}?random=${randomString}`}
+                alt=""
+                className="h-full w-full cursor-pointer rounded-full"
+              />
+            </div>
             <span className="hidden text-white min-[1264px]:flex">Profile</span>
           </div>
         </div>
