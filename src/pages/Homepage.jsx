83470befb2ae8bfc5998d8f61c09e0b12865ec76
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-02 10:43:29 +0200

    [Style]: Finalizing the HHomepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,10 +6,10 @@ import Suggested from "../components/Suggested/Suggested";
 
 const Homepage = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end min-[1264px]:flex-row min-[1264px]:items-start min-[1264px]:p-0">
+    <div className="bg-blue-300 flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end min-[1264px]:flex-row min-[1264px]:items-start min-[1264px]:p-0">
       <NavBar />
-      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%] min-[1160px]:justify-around min-[1160px]:items-start min-[1264px]:h-full">
-        <div className="w-full flex flex-col items-center min-[1160px]:w-fit">
+      <div className="bg-green-300 flex w-full flex-col items-center md:h-full md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%] min-[1160px]:justify-around min-[1160px]:items-start min-[1264px]:h-full">
+        <div className="bg-slate-300 md:h-full w-full flex flex-col items-center min-[1160px]:w-fit">
           <Header />
           <Stories />
           <Posts />
