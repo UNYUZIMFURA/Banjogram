29d572a40926e0494a652a6e9c1def61df822a07
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-05 21:48:48 +0200

    [Feat]: Banjogram Homepage complete!

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ import Suggested from "../components/Suggested/Suggested";
 
 const Homepage = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end min-[1264px]:flex-row min-[1264px]:items-start min-[1264px]:justify-around min-[1264px]:p-0 2xl:justify-between">
+    <div className="flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end min-[1264px]:flex-row min-[1264px]:items-start min-[1264px]:justify-around min-[1264px]:p-0 2xl:justify-between 2xl:pr-7">
       <NavBar />
       <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%] xl:w-[80%] min-[1160px]:justify-around min-[1160px]:items-start min-[1264px]:h-full min-[1264px]:pt-8 min-[1500px]:justify-center min-[1500px]:gap-[5rem]">
         <div className="w-full flex flex-col items-center min-[1160px]:w-fit">
