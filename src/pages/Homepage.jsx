3a1b08df367b09e9bb1a5e2c5882f6682a99d4f5
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-05 21:19:28 +0200

    [Style]: Fixing some layouts!

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-black md:pt-8 md:items-center md:justify-start min-[1160px]:items-end min-[1264px]:flex-row min-[1264px]:items-start min-[1264px]:justify-around min-[1264px]:p-0">
       <NavBar />
-      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%] xl:w-[80%] min-[1160px]:justify-around min-[1160px]:items-start min-[1264px]:h-full min-[1264px]:pt-8 bg-red-20">
+      <div className="flex w-full flex-col items-center md:w-[85%] min-[1160px]:flex-row min-[1160px]:w-[90%] xl:w-[80%] min-[1160px]:justify-around min-[1160px]:items-start min-[1264px]:h-full min-[1264px]:pt-8 min-[1500px]:justify-center min-[1500px]:gap-[5rem]">
         <div className="w-full flex flex-col items-center min-[1160px]:w-fit">
           <Header />
           <Stories />
