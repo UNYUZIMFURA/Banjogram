0922994e7d0a79ae5e33b8e231dfb726af9d19dc
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-08 14:14:36 +0200

    [Add]: Random change

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -43,8 +43,7 @@ const Post = () => {
       </div>
       <div className="flex items-center px-2 py-1 scrollbar-hide">
         <p className="text-sm text-white">
-          <span className="font-bold cursor-pointer">433_football</span> <span className="text-[#bed1e9] cursor-pointer">@vinijr</span> has the same amount of CL goals for
-          Real Madristas <span className="text-[#bed1e9] cursor-pointer">@leo_messi</span>. News came straight from <span className="text-[#bed1e9] cursor-pointer">#banjo</span>
+          <span className="font-bold cursor-pointer">433_football</span> <span className="text-[#bed1e9] cursor-pointer">@leo_messi</span> has moved to Inter Miami just as<span className="text-[#bed1e9] cursor-pointer"> @cristiano</span> moved to Al Nassr, news straight from <span className="text-[#bed1e9] cursor-pointer"> #banjo</span>
         </p>
       </div>
       <div className="flex flex-col gap-2 border-b border-[#303030] bg-black px-2 pb-9 pt-2 text-white">
