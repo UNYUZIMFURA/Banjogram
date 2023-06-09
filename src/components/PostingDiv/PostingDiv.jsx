31a0ca828e36e5b47fb17627c036fe0cd0ae2e88
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-09 09:11:04 +0200

    [Style]: Style the Posting Div

--- src/components/PostingDiv/PostingDiv.jsx
+++ src/components/PostingDiv/PostingDiv.jsx
@@ -11,13 +11,13 @@ const PostingDiv = () => {
   return (
     <div className="fixed h-screen w-screen flex items-center justify-center bg-[#00000098]" onClick={()=> closeDiv()}>
     <div className="fixed top-1/4 flex h-[50vh] w-full flex-col justify-between bg-[#272727]">
-      <div className="flex h-[2.8rem] w-full items-center justify-center border-b border-[#3E404] font-semibold text-white">
-        <span>Create new post</span>
+      <div className="flex h-[2.8rem] w-full items-center justify-center border-b border-[#3E4042] font-semibold text-white">
+        <span className="text-md">Create new post</span>
       </div>
-      <div className="text-white flex flex-grow flex-col items-center justify-center gap-3 ">
+      <div className="text-white flex flex-grow flex-col items-center justify-center gap-5 ">
         <FiImage size={60} />
-        <span>Drag photos and videos here</span>
-        <button className="bg-[rgb(0,149,246)] font-medium text-sm px-2 py-2 rounded-lg outline-none">Select from computer</button>
+        <span className="text-lg font-semibold">Drag photos and videos here</span>
+        <button className="bg-[rgb(0,149,246)] font-semibold text-sm px-2 py-2 rounded-lg outline-none">Select from computer</button>
       </div>
     </div>
     </div>
