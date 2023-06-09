f56cbe8da8f0d0036822ab199a2c7a62e86fa883
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-09 10:39:39 +0200

    [Style]: Start working of Posting Div

--- src/components/PostingDiv/PostingDiv.jsx
+++ src/components/PostingDiv/PostingDiv.jsx
@@ -10,13 +10,13 @@ const PostingDiv = () => {
 
   return (
     <div className="fixed h-screen w-screen flex items-center justify-center bg-[#00000098]" onClick={()=> closeDiv()}>
-    <div className="fixed top-1/4 flex h-[50vh] w-full flex-col justify-between bg-[#272727]">
+    <div className="fixed top-1/4 flex h-[47vh] w-full flex-col justify-between bg-[#272727] min-[350px]:w-[21.7rem] rounded-xl" >
       <div className="flex h-[2.8rem] w-full items-center justify-center border-b border-[#3E4042] font-semibold text-white">
         <span className="text-md">Create new post</span>
       </div>
       <div className="text-white flex flex-grow flex-col items-center justify-center gap-5 ">
         <FiImage size={60} />
-        <span className="text-lg font-semibold">Drag photos and videos here</span>
+        <span className="text-lg">Drag photos and videos here</span>
         <button className="bg-[rgb(0,149,246)] font-semibold text-sm px-2 py-2 rounded-lg outline-none">Select from computer</button>
       </div>
     </div>
