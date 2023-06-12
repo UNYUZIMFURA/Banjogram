ca8d4b778603a19f53dbe5061f50eec31814335a
UNYUZIMFURA <banjoflylive@gmail.com>
2023-06-12 15:18:43 +0200

    [Style]: ...

--- src/components/PostingDiv/PostingDiv.jsx
+++ src/components/PostingDiv/PostingDiv.jsx
@@ -16,16 +16,17 @@ const PostingDiv = () => {
 
   return (
     <div className="z-[1] top-0 fixed  h-screen w-screen flex items-center justify-center bg-[#00000098]" onClick={(e)=> closeUploadDiv(e)} id="uploadDivHolder">
-    <div className="flex h-[48vh] w-full flex-col justify-between bg-[#272727] min-[350px]:w-[21.7rem] rounded-xl min-[720px]:w-[50%] min-[720px]:h-[55vw] min-[970px]:h-[40rem] min-[970px]:w-[37rem]" >
+    <form className="flex h-[48vh] w-full flex-col justify-between bg-[#272727] min-[350px]:w-[21.7rem] rounded-xl min-[720px]:w-[50%] min-[720px]:h-[55vw] min-[970px]:h-[40rem] min-[970px]:w-[37rem]" >
       <div className="flex h-[2.8rem] w-full items-center justify-center border-b border-[#3E4042] font-semibold text-white">
         <span className="text-md">Create new post</span>
       </div>
       <div className="text-white flex flex-grow flex-col items-center justify-center gap-4 ">
            <SvgElement />
         <span className="text-lg">Drag photos and videos here</span>
-        <button className="bg-[rgb(0,149,246)] font-semibold text-sm px-2 py-2 rounded-lg outline-none">Select from computer</button>
+        <input type="file" id="imgUploader" className="hidden"/>
+        <label className="bg-[rgb(0,149,246)] font-semibold text-sm px-2 py-2 rounded-lg outline-none cursor-pointer" for="imgUploader">Select from computer</label>
       </div>
-    </div>
+    </form>
     </div>
   );
 };
