5c88bf828e03029ab1e2c0bcf99a7c4ae44b596b
UNYUZIMFURA <banjoflylive@gmail.com>
2023-07-19 14:16:47 +0200

    [Refactor]: Use of Single Component in Uploading for All Devices

--- src/components/PostingDiv/PostingDiv.jsx
+++ src/components/PostingDiv/PostingDiv.jsx
@@ -5,8 +5,6 @@ import SvgElement from "./SvgElement";
 import { FiArrowLeft } from "react-icons/fi";
 
 const PostingDiv = () => {
-  const [mobileView, setMobileView] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
   const dispatch = useDispatch();
   const [nextClicked, setNextClicked] = useState(false);
   const [image, setImage] = useState("");
@@ -107,57 +105,7 @@ const PostingDiv = () => {
   ]);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-
-    const debounceHandleResize = debounce(handleResize, -100);
-
-    window.addEventListener("resize", debounceHandleResize);
-
-    return () => {
-      window.removeEventListener("resize", debounceHandleResize);
-    };
-  }, []);
-
-  const debounce = (fn, delay) => {
-    let timerId;
-    return () => {
-      clearTimeout(timerId);
-      timerId = setTimeout(() => {
-        fn();
-      }, delay);
-    };
-  };
-
-  useEffect(() => {
     const addPostDetails = () => {
-      if (window.innerWidth < 768 || width < 768) {
-        const postingDiv = document.getElementById("posting-div");
-        const imgDiv = document.getElementById("img-div");
-        const addPostDetails = document.getElementById("add-post-details");
-        const userImage = document.getElementById("user-image");
-
-        /* Removing Previously Made DOM Manipulations of ->
-           Enabling User to Add Caption in Desktop Version and Larger Breakpoints
-           This will be removed once the user resizes back to Mobile Version and Smaller Breakpoints
-        */
-
-        if (window.innerWidth >= 350) {
-          postingDiv.style.width = "21.7rem";
-          // postingDiv.style.height = "48vh"
-        } else if (window.innerWidth >= 768) {
-          postingDiv.style.width = "50%";
-          // postingDiv.style.height = "48vh"
-        }
-
-        imgDiv.style.width = "100%";
-        imgDiv.style.borderRadius = "0";
-        userImage.style.borderRadius = "0";
-        addPostDetails.style.display = "none";
-        setMobileView(true);
-      } else {
-        setMobileView(false);
         if (nextClicked) {
           const postingDiv = document.getElementById("posting-div");
           const imgDiv = document.getElementById("img-div");
@@ -167,7 +115,7 @@ const PostingDiv = () => {
 
           postingDiv.style.width = window.innerWidth >= 1100 ? "68rem" : "95%";
           postingDiv.style.borderRadius = "0.5rem";
-          imgDiv.style.width = window.innerWidth > 1100 ? "70%" : "50%";
+          imgDiv.style.width = window.innerWidth > 1100 ? "67%" : "50%";
           imgDiv.style.borderBottomLeftRadius = "0.5rem";
           userImage.style.borderBottomLeftRadius = "0.5rem";
           childOne.style.flexDirection = "row";
@@ -175,9 +123,8 @@ const PostingDiv = () => {
           addPostDetails.style.borderBottomRightRadius = "0.5rem";
         }
       }
-    };
     addPostDetails();
-  }, [width, nextClicked, mobileView]);
+  }, [nextClicked]);
 
   const closeDiv = () => {
     dispatch(hideDiv());
@@ -187,14 +134,6 @@ const PostingDiv = () => {
     if (e.target.id === "upload-div-holder") {
       closeDiv();
     }
-  };
-
-  const closeCaptionDiv = (e) => {
-    if (e.target.id !== "mobile-caption-div") return;
-    const mobileCaptionDiv = document.getElementById("mobile-caption-div");
-    mobileCaptionDiv.style.display = "none";
-    setNextClicked(false);
-    setMobileView(false);
   };
 
   const handleImage = (e) => {
@@ -229,19 +168,6 @@ const PostingDiv = () => {
         className="flex h-[48vh] w-full flex-col justify-between rounded-xl bg-[#272727] min-[350px]:w-[21.7rem] min-[720px]:h-[55vw] min-[720px]:w-[50%] min-[970px]:h-[40rem] min-[970px]:w-[37rem] min-[1905px]:h-[48rem] min-[1905px]:w-[45rem]"
         id="posting-div"
       >
-        {mobileView && nextClicked ? (
-          <div
-            className="fixed left-0 top-0 z-[1] flex h-screen w-screen items-center justify-center bg-[#00000077]"
-            id="mobile-caption-div"
-            onClick={(e) => closeCaptionDiv(e)}
-          >
-            <textarea
-              id=""
-              placeholder="Write a caption..."
-              className="h-[20vh] w-[20rem] rounded-lg bg-[#272727] px-4 py-4 text-white outline-none"
-            />
-          </div>
-        ) : null}
         <div className="flex h-[2.8rem] w-full items-center justify-between border-b border-[#3E4042] px-4 font-semibold text-white">
           <FiArrowLeft size={25} id="arrow-left" className="hidden" />
           <span className="text-md mx-auto">Create new post</span>
@@ -289,7 +215,7 @@ const PostingDiv = () => {
           </div>
           <div
             id="add-post-details"
-            className="hidden h-full w-1/2 flex-col items-start gap-5 bg-[#272727] p-4 pl-5 pr-2 min-[1100px]:w-[30%]"
+            className="hidden h-full w-1/2 flex-col items-start gap-5 bg-[#272727] p-4 pl-5 pr-2 min-[1100px]:w-[33%]"
           >
             <div className="flex items-center gap-3 py-1">
               <div className="h-8 w-8 cursor-pointer rounded-full">
