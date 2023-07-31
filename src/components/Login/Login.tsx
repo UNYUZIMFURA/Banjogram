3b7e35087a31188a9a83219d8eef1301c3e88119
UNYUZIMFURA <banjoflylive@gmail.com>
2023-07-31 11:04:03 +0200

    ...

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -5,8 +5,8 @@ const Login = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex h-[80vh] w-[95%] flex-col items-center gap-2">
-        <div className="flex w-full flex-col items-center pt-5 gap-5 border">
-          <span className="cursor-pointer font-satisfy text-[2rem] text-black">
+        <div className="flex w-full flex-col items-center pt-5 gap-4 border">
+          <span className="cursor-pointer font-bold font-satisfy text-[2.3rem] text-black">
             Banjogram
           </span>
           <div className="flex flex-col py-5">
@@ -20,7 +20,7 @@ const Login = () => {
                 className="border bg-[rgb(250,250,250)] text-ellipsis p-2 outline-none placeholder:text-xs"
               />
             </div>
-            <div className="bg-[green flex flex-col items-center gap-3 py-2">
+            <div className="bg-[green flex flex-col items-center gap-4 py-2">
               <button className="w-full cursor-pointer rounded-lg bg-[rgb(0,149,246)] py-[0.4rem] font-semibold text-white text-sm ">
                 Log in
               </button>
@@ -30,26 +30,29 @@ const Login = () => {
                 <div className="h-[1px] w-[5.7rem] bg-[rgb(219,219,219)]"></div>
               </div>
             </div>
-            <div className="py-4 flex w-full flex-col items-center justify-center gap-3">
+            <div className="py-3 flex w-full flex-col items-center justify-center gap-4">
               <span className="flex items-center gap-2">
-                <IoLogoFacebook fill="blue" size={20} />
-                <span className="text-sm font-semibold text-[#385185]">
+                <IoLogoFacebook fill="#385185" size={20} />
+                <span className="text-sm font-semibold text-[#385185] cursor-pointer">
                   Log in with Facebook
                 </span>
               </span>
-              <span className="text-xs text-[rgb(0,55,107)]">
+              <span className="text-xs text-[rgb(0,55,107)] cursor-pointer">
                 Forgot password?
               </span>
             </div>
           </div>
         </div>
-        <div className="flex w-full justify-center gap-2 border p-4 text-sm">
+        <div className="flex w-full justify-center gap-2 border py-5 px-4 text-sm">
           <span>Don't have an account?</span>
           <span className="cursor-pointer text-[rgb(0,149,246)] font-semibold">Sign up</span>
         </div>
-        <div className="flex w-full gap-2 justify-around">
-          <img src="/apple_store.png" alt="" className="h-9" />
-          <img src="/play_store.png" alt="" className="h-9" />
+        <div className="flex items-center gap-4 py-2 w-full flex-col text-sm">
+          <span>Get the app.</span>
+          <div className="flex w-full gap-2 justify-around">
+          <img src="/apple_store.png" alt="" className="h-9 cursor-pointer" />
+          <img src="/play_store.png" alt="" className="h-9 cursor-pointer" />
+          </div>
         </div>
       </div>
     </div>
