31c76b36cf0e0b4ecb8df5f1a2ec0ff084bfdd09
UNYUZIMFURA <banjoflylive@gmail.com>
2023-09-08 21:10:29 +0200

    [Add]: Add extra content for posts

--- src/components/Posts/Post.tsx
+++ src/components/Posts/Post.tsx
@@ -2,15 +2,12 @@ import "index.css";
 import React, { useState, useEffect } from "react";
 import PostHeader from "./PostHeader";
 
-import {
-  HeartSvg,
-  CommentSvg,
-  MessageSvg,
-  SaveSvg,
-} from "../../SVGs/SvgElements";
+import { HeartSvg, CommentSvg, MessageSvg } from "../../SVGs/SvgElements";
 
 const Post = (props) => {
   const [randomNumsArr, setRandomNumsArr] = useState<number[]>([]);
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
 
   function generateRand() {
     const randomString = Math.random();
@@ -24,36 +21,82 @@ const Post = (props) => {
   return (
     <div className="mt-4 flex min-h-[80vh] w-screen flex-col min-[470px]:w-[29.5rem]">
       <PostHeader username={props.username} />
-      <div className="flex h-[62%] w-full cursor-pointer rounded-[0.2rem]">
+      <div className="load flex h-[62%] w-full cursor-pointer rounded-[0.2rem]">
         <img
           src={`${process.env.REACT_APP_PRIMARY_IMAGES_ENDPOINT}/?${props.category}`}
           alt=""
-          className="load w-full rounded-t-[0.2rem] object-cover"
+          className="w-full rounded-t-[0.2rem] object-cover"
           loading="lazy"
         />
       </div>
       <div className="flex w-full flex-col gap-2 p-3">
         <div className="flex items-center justify-between gap-2 py-2">
           <div className="flex gap-4">
-            <HeartSvg cursor="pointer" />
+            <div
+              className="cursor-pointer"
+              onClick={() => setLiked((prevState) => !prevState)}
+            >
+              <svg
+                aria-label=""
+                cursor="pointer"
+                fill={liked ? "rgb(255,48,64)" : "rgb(245,245,245)"}
+                height="24"
+                role="img"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <title>Like</title>
+                <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
+              </svg>
+            </div>
             <CommentSvg />
             <MessageSvg />
           </div>
-          <SaveSvg />
+          <div
+            className="cursor-pointer"
+            onClick={() => setSaved((prevState) => !prevState)}
+          >
+            <svg
+              aria-label="Save"
+              cursor="pointer"
+              color={saved ? "rgb(255,48,64)" : "rgb(245,245,245)"}
+              height="24"
+              role="img"
+              viewBox="0 0 24 24"
+              width="24"
+            >
+              <title>Save</title>
+              <polygon
+                fill="none"
+                points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              ></polygon>
+            </svg>
+          </div>
         </div>
         <div className="flex items-center gap-2">
-          <div className="loading h-5 w-5 cursor-pointer rounded-full border">
-            <img
-              src={`${process.env.REACT_APP_SECONDARY_IMAGES_ENDPOINT}?random=${randomNumsArr[1]}`}
-              alt=""
-              className="rounded-full"
-              loading="lazy"
-            />
-          </div>
-          <p className="text-sm font-medium text-white">
-            Liked by <span className="cursor-pointer font-bold">cristiano</span>{" "}
-            and <span className="cursor-pointer font-bold">75.6M others</span>
-          </p>
+          {!props.likes && (
+            <div className="load h-5 w-5 cursor-pointer rounded-full border">
+              <img
+                src={`${process.env.REACT_APP_SECONDARY_IMAGES_ENDPOINT}?random=${randomNumsArr[1]}`}
+                alt=""
+                className="rounded-full"
+                loading="lazy"
+              />
+            </div>
+          )}
+          {props.likes ? (
+            <p className="text-sm font-bold text-white">{props.likes} likes</p>
+          ) : (
+            <p className="text-sm font-medium text-white">
+              Liked by{" "}
+              <span className="cursor-pointer font-bold">cristiano</span> and{" "}
+              <span className="cursor-pointer font-bold">75.6M others</span>
+            </p>
+          )}
         </div>
       </div>
       <div className="flex items-center px-2 py-1 text-white scrollbar-hide">
@@ -78,11 +121,11 @@ const Post = (props) => {
       </div>
       <div className="flex flex-col gap-2 border-b border-[#303030] bg-black px-2 pb-9 pt-2 text-white">
         <span className="cursor-pointer text-sm text-[#9b9b9b]">
-          View all 10 comments
+          {`View all ${props.comments} comments`}
         </span>
         <p className="flex items-center justify-between text-sm">
           <span>
-            <span className="font-bold">ihirwe_olis</span> USA is still #1
+            <span className="font-bold">joy_kevin</span> The images make it look real!
           </span>
           <HeartSvg />
         </p>
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,21 +2,94 @@ import Post from "./Post";
 
 const Posts = () => {
   const posts = [
-    { id: 1, username: "leomessi", category: "football" },
-    { id: 2, username: "rocky_kimomo", caption: "Agasobanuye gashya, Hear Me! Gera kuri website www.agasobanuyefilms.com, Weekend niho bibera!", category: "movie" },
-    { id: 3, username: "iva1nqueur", caption: "An unforgettable summer in Seoul.", category: "travel"},
-    { id: 4, username: "element_eleeeh", caption: "GIANTS OF AFRICA live in BK Arena!", category: "music"},
-    { id: 5, username: "k.mbappe", caption: "Chilling and enjoying the vibes in Miami in the best Hotels.", category: "vacation" },
-    { id: 6, username: "_keliaruzindana_", caption: "<<Rejoice in the Lord, Always!>>", category: "pray"},
-    { id: 7, username: "meddyonly", caption: "BLESSED, Dropping Anytime from now!", category: "clock" },
-    { id: 8, username: "igiheofficial", caption: "Zigama, hamwe na Cogebanque.", category: "bank"},
-    { id: 9, username: "madebeats_official", caption: "Bald is the new black.", category: "bald" },
-    { id: 10, username: "bkarenarw", caption: "Heads Up!!! Nigerian Superstar davido will be performing this Saturday!", category: "live performance" },
+    { id: 1, username: "leomessi", category: "Soccer", comments: "20.5K" },
+    {
+      id: 2,
+      username: "rocky_kimomo",
+      caption:
+        "Agasobanuye gashya, Hear Me! Gera kuri website www.agasobanuyefilms.com, Weekend niho bibera!",
+      category: "Movie",
+      likes: "9,460",
+      comments: "1,480",
+    },
+    {
+      id: 3,
+      username: "iva1nqueur",
+      caption: "An unforgettable summer in Seoul.",
+      category: "South Korea",
+      likes: "77",
+      comments: "30",
+    },
+    {
+      id: 4,
+      username: "element_eleeeh",
+      caption: "GIANTS OF AFRICA, live in BK Arena!",
+      category: "Pianist",
+      likes: "15.1K",
+      comments: "2,400",
+    },
+    {
+      id: 5,
+      username: "k.mbappe",
+      caption: "Chilling and enjoying the vibes on Miami beaches!",
+      category: "Miami Beach",
+      likes: "3.3M",
+      comments: "20K",
+    },
+    {
+      id: 6,
+      username: "_keliaruzindana_",
+      caption: "<<Rejoice in the Lord, Always!>>",
+      category: "Pray",
+      likes: "10K",
+      comments: "3,400",
+    },
+    {
+      id: 7,
+      username: "meddyonly",
+      caption: "BLESSED, Dropping Anytime from now!",
+      category: "Clock",
+      likes: "20K",
+      comments: "8,050",
+    },
+    {
+      id: 8,
+      username: "igiheofficial",
+      caption: "Zigama, hamwe na Cogebanque!",
+      category: "Savings",
+      likes: "3,800",
+      comments: "1,200",
+    },
+    {
+      id: 9,
+      username: "madebeats_official",
+      caption: "Bald is the new black.",
+      category: "Bald",
+      likes: "1,500",
+      comments: "505",
+    },
+    {
+      id: 10,
+      username: "bkarenarw",
+      caption:
+        "Heads Up!!! Nigerian Superstar davido will be performing this Saturday!",
+      category: "Live Performance",
+      likes: "9.2K",
+      comments: "5,500",
+    },
   ];
   return (
     <div className="flex h-[70vh] w-full flex-col items-center gap-12 md:h-[78vh]">
       {posts.map((post) => (
-        <Post id={post.id} key={post.id} username={post.username} caption={post.caption} category={post.category}/>
+        <Post
+          id={post.id}
+          key={post.id}
+          username={post.username}
+          caption={post.caption}
+          category={post.category}
+          likes={post.likes}
+          comments={post.comments}
+        />
       ))}
     </div>
   );
